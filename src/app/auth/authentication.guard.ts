8f8e014acb5f466a758e2ac8f21f8835d6f6d9d7
Shiro Angel <hoangtuananh.ub.tq@gmail.com>
2022-05-22 21:55:13 +0700

    Pending Route Guard Home

--- src/app/auth/authentication.guard.ts
+++ src/app/auth/authentication.guard.ts
@@ -4,17 +4,12 @@ import { Logger } from '@app/@shared';
 import { OidcSecurityService } from 'angular-auth-oidc-client';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { CredentialsService } from './credentials.service';
 
 const log = new Logger('AuthenticationGuard');
 
 @Injectable({ providedIn: 'root' })
 export class AuthenticationGuard implements CanActivate {
-  constructor(
-    private router: Router,
-    private credentialsService: CredentialsService,
-    private oidcSecurityService: OidcSecurityService
-  ) {}
+  constructor(private router: Router, private oidcSecurityService: OidcSecurityService) {}
 
   // canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
   // if (this.credentialsService.isAuthenticated()) {
@@ -26,39 +21,51 @@ export class AuthenticationGuard implements CanActivate {
   // return false;
   // }
 
-  // canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree > {
-  // return this.oidcSecurityService.isAuthenticated$.pipe(
-  //     map(({ isAuthenticated }) => {
-  //       // allow navigation if authenticated
-  //       if (isAuthenticated) {
-  //         return true;
-  //       }
-  //         log.info('isAuthen: '+isAuthenticated)
-  //       // redirect if not authenticated
-  //       this.router.navigate(['/login'], { queryParams: { redirect: state.url }, replaceUrl: true });
-  //       return false;
-  //     })
-  // );
-  // }
-
-  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | boolean {
-    const expectedRoles = next.data['expectedRoles'] as Array<string>;
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
     return this.oidcSecurityService.isAuthenticated$.pipe(
-      map((isAuthenticated) => {
-        if (!isAuthenticated) {
-          log.info('IsAuthen: ' + isAuthenticated);
-          this.router.navigate(['/login'], { queryParams: { redirect: state.url }, replaceUrl: true });
+      map(({ isAuthenticated }) => {
+        if (isAuthenticated) {
+          this.router.navigate(['/login'], {
+            queryParams: {
+              redirect: state.url,
+            },
+            replaceUrl: true,
+          });
           return false;
         }
-        if (this.checkTokenRoles(this.oidcSecurityService.getAccessToken(), expectedRoles)) {
-          this.router.navigate(['/login'], { queryParams: { redirect: state.url }, replaceUrl: true });
-          return false;
-        } else {
-          return true;
-        }
+        log.info('isAuthen: ' + isAuthenticated);
+        // redirect if not authenticated
+        return true;
       })
     );
   }
+
+  // canActivate(next : ActivatedRouteSnapshot, state : RouterStateSnapshot): Observable < boolean > | boolean {
+  //     const expectedRoles = next.data['expectedRoles'] as Array < string >;
+  //     return this.oidcSecurityService.isAuthenticated$.pipe(map((isAuthenticated) => {
+  //         if (!isAuthenticated) {
+  //             log.info('IsAuthen: ' + isAuthenticated);
+  //             this.router.navigate(['/login'], {
+  //                 queryParams: {
+  //                     redirect: state.url
+  //                 },
+  //                 replaceUrl: true
+  //             });
+  //             return false;
+  //         }
+  //         if (this.checkTokenRoles(this.oidcSecurityService.getAccessToken(), expectedRoles)) {
+  //             this.router.navigate(['/login'], {
+  //                 queryParams: {
+  //                     redirect: state.url
+  //                 },
+  //                 replaceUrl: true
+  //             });
+  //             return false;
+  //         } else {
+  //             return true;
+  //         }
+  //     }));
+  // }
 
   checkTokenRoles(token: any, expectedRoles: Array<string>): boolean {
     if (token) {
